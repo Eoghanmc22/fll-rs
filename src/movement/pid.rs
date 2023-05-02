2323276a0af04ba1ec4c79a98eee897ba0c20b42
Eoghanmc22 <eoghanmc@icloud.com>
2023-05-02 15:24:18 -0400

    Make Pid Dependent on time

--- src/movement/pid.rs
+++ src/movement/pid.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 #[derive(Copy, Clone, Debug)]
 pub struct PidConfig {
     /// How strongly to correct current error
@@ -16,30 +18,30 @@ pub struct PidConfig {
 /// Implementation of PID algorithm
 #[derive(Clone, Debug)]
 pub struct PidController {
-    pid: PidConfig,
-
+    interval: Duration,
     integral: f32,
     last_error: f32,
 }
 
 impl PidController {
-    pub fn new(pid: PidConfig) -> Self {
+    pub fn new(interval: Duration) -> Self {
         PidController {
-            pid,
+            interval,
             integral: 0.0,
             last_error: 0.0,
         }
     }
 
-    pub fn update(&mut self, error: f32) -> (f32, (f32, f32, f32)) {
-        let cfg = &self.pid;
+    pub fn update(&mut self, error: f32, config: PidConfig) -> PidResult {
+        let cfg = config;
+        let interval = self.interval.as_secs_f32();
 
-        self.integral += error;
+        self.integral += error * interval;
         self.integral = self.integral.clamp(-cfg.max_i, cfg.max_i);
 
         let proportional = error;
         let integral = self.integral;
-        let derivative = error - self.last_error;
+        let derivative = (error - self.last_error) / interval;
 
         self.last_error = error;
 
@@ -47,6 +49,19 @@ impl PidController {
         let i = cfg.ki * integral;
         let d = cfg.kd * derivative;
 
-        (p + i + d, (p, i, d))
+        PidResult { p, i, d }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct PidResult {
+    pub p: f32,
+    pub i: f32,
+    pub d: f32,
+}
+
+impl PidResult {
+    pub fn corection(&self) -> f32 {
+        self.p + self.i + self.d
     }
 }
